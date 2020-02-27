37db43f74e04f9b8b37a7c9f2b550c3baddc846d
aditi <aditi.desai298@gmail.com>
2020-02-27 17:23:22 +0530

    [aditi] added http service

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,43 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { headersToString } from 'selenium-webdriver/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpServiceService {
+export class HttpService {
+  constructor(private _http: HttpClient) { }
 
-  postWithoutHeader(url: any, uploadData: any) {
-    throw new Error("Method not implemented.");
+  
+  post(url: any, body: any, options: any): Observable<any> {
+    return this._http.post(url, body, options);
   }
 
-  baseUrl = environment.apiUrl;
-  token = localStorage.getItem('FundooToken');
-
-  httpOptions = new HttpHeaders().set('token', this.token)
-
-  constructor(private http: HttpClient) { }
-
-  public postUser(options) {
-
-    //console.log("httpService",options.url);
-
-    console.log("this.baseurl", this.baseUrl);
-    console.log("options.data", options.data);
-
-
-
-    return this.http.post(this.baseUrl + options.url, options.data);
+  get(url: any, options: any): Observable<any> {
+    return this._http.get(url, options);
   }
-  public post(options) {
-    console.log('token', this.token)
 
-    let url = this.baseUrl + options.url;
-    console.log("baseUrl", url)
-
-    console.log("options", options.data)
-    return this.http.post(this.baseUrl + options.url, options.data, { headers: this.httpOptions });
-
+  put(url: any, body: any, options: any): Observable<any> {
+    return this._http.put(url, body, options);
   }
+  delete(url: any, options: any):Observable<any>{
+    return this._http.delete(url, options);
+  }
+
 }
--- src/app/services/userservice.service.ts
+++ src/app/services/userservice.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpService } from '../services/http-service.service'
+import { Observable } from 'rxjs';
+
 
 import { UserModel} from '../model/user-model'
 import { RegisterModel } from '../model/register-model';
@@ -13,14 +16,14 @@ export class UserserviceService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private _http: HttpClient, private httpservice: HttpService) { }
 
-  public login(user: UserModel){
+  public login(user: UserModel):Observable<any>{
     console.log(user);
-    return this.http.post(`http://localhost:8084/user/login`, user, this.httpOptions);
+    return this.httpservice.post(`http://localhost:8084/user/login`, user, this.httpOptions);
   }
 
-  public registration(user: RegisterModel) {
-    return this.http.post('http://localhost:8084/user/registration', user, this.httpOptions);
+  public registration(user: RegisterModel):Observable<any> {
+    return this.httpservice.post('http://localhost:8084/user/registration', user, this.httpOptions);
   }
 }
